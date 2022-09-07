5facd245a279a5d7797325f6e2c028561bd69b2c
unknown <dolgopolov.1999@list.ru>
2022-09-07 14:15:07 +0300

    First Commit

---
+++ noWork.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func main() {
+	phrase := readString()
+
+	// 1. Разбейте фразу на слова, используя `strings.Fields()`
+	// 2. Возьмите первую букву каждого слова и приведите
+	//    ее к верхнему регистру через `unicode.ToUpper()`
+	// 3. Если слово начинается не с буквы, игнорируйте его
+	//    проверяйте через `unicode.IsLetter()`
+	// 4. Составьте слово из получившихся букв и запишите его
+	//    в переменную `abbr`
+	// ...
+	words := strings.Fields(phrase)
+
+	var abbr string
+
+	for i := 0; i < len(words); i++ {
+		for idx, char := range words[i] {
+			if idx == 0 && unicode.IsLetter(char) {
+				//char = unicode.ToUpper(char)
+				abbr += string(char)
+			} else {
+				continue
+			}
+		}
+	}
+	//вот этот кусок не работает так, как должен
+	for _, char := range abbr {
+		unicode.ToUpper(char)
+		// char = unicode.ToUpper(char) тоже не работает 
+	}
+
+	fmt.Println(abbr)
+}
+
+// ┌─────────────────────────────────┐
+// │ не меняйте код ниже этой строки │
+// └─────────────────────────────────┘
+
+// readString читает строку из `os.Stdin` и возвращает ее
+func readString() string {
+	rdr := bufio.NewReader(os.Stdin)
+	str, _ := rdr.ReadString('\n')
+	return str
+}
